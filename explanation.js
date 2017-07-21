f3eb473e4d1d02192b289867280be420eaea7fce
Martin Neuschl <martin.neuschl1@gmail.com>
2017-07-21 07:44:13 +0200

    přidáno více kódu

---
+++ explanation.js
@@ -0,0 +1,30 @@
+// Explanation
+var favColor = 'blue';
+var myFavColor = ['blue', 'red', 'green']; // array
+var numOfFavColors = 3; // číslo
+var hasGotFavColors = true; // boolean
+var richObject = {
+	firstName: 'Martin',
+	lastName: 'Neuschl',
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 30,
+	ismale: true
+};
+
+var fullname = function (firstName, lastName) {
+	return firstName ++ lastName;
+}
+richObject.ismale
+
+var area = function (width, height) {
+	return width * height;
+}
+
+var whatIsMyfavColor = function () {
+
+	return 'blue';
+}
+
+var doSomething = function () {
+	console.log('Do something!!!');
+}
